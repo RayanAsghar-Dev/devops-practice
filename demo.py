1bde987bb4be4e19e7316e1e083bb3fc5df1c1e7
RayanAsghar-Dev <rayanrao984@gmail.com>
2026-05-15 14:16:18 +0500

    adii

--- demo.py
+++ demo.py
@@ -1,13 +1,1 @@
-def  my_function ():
-    a=5 a=5
-    return d
-    my_function()
-my gdgd 
-d dhfjdd
- djhfjd
- test ()
-  ada 
-    db66
-    66
-    67
-    777 
+print ("hello rayan flakr8 is working")
